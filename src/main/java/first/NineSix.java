bf0d615fd2243fb10c421339dd30176867f7d823
machitao <1035937382@qq.com>
2021-06-26 22:51:45 +0800

    perf:补充96题方法二

--- src/main/java/first/NineSix.java
+++ src/main/java/first/NineSix.java
@@ -17,7 +17,7 @@ public class NineSix {
     }
 
     /**
-     * 和95题同理
+     * 方法一：和95题同理递归，但是会超时
      * @param start
      * @param end
      * @return
@@ -32,6 +32,29 @@ public class NineSix {
         }
         return count;
     }
+
+    /**
+     * 方法二：动态规划
+     * @param n
+     * @return
+     */
+    private int dp(int n){
+        // 元素数量为n的二叉树会有多少种可能
+        int[] dpArray = new int[n+1];
+        // 空树的话是一种
+        dpArray[0] = 1;
+        // 一个元素的话也是一种
+        dpArray[1] = 1;
+
+        // 元素个数的遍历
+        for (int i = 2; i < n+1; i++){
+            for (int j = 1; j <= i; j++){
+                dpArray[i] += dpArray[j-1] * dpArray[i-j];
+            }
+        }
+        return dpArray[n];
+    }
+
 
     private static class TreeNode{
         int val;
