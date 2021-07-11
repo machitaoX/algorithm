7fb774cf11772ad3d9f9d2629d956ae04ad40013
machitao <1035937382@qq.com>
2021-07-11 23:11:24 +0800

    feat:添加101题解

---
+++ src/main/java/first/OneZeroOne.java
@@ -0,0 +1,45 @@
+package first;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author machitao
+ * @date 2021/7/5
+ * @description
+ */
+public class OneZeroOne {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return (left.val == right.val) && check(left.left, right.right) && check(left.right, right.left);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
