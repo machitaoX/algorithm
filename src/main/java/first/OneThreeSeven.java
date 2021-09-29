3972ab5d3a4cc3fe7ccce8723f7e3add9eb030b1
machitao <1035937382@qq.com>
2021-09-29 23:32:04 +0800

    feat:添加136题题解

---
+++ src/main/java/first/OneThreeSeven.java
@@ -0,0 +1,24 @@
+package first;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author machitao
+ * @date 2021/9/29
+ * @description
+ */
+public class OneThreeSeven {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
