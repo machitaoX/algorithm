08e8ef2ae3e218a538fda132d925231054e8f767
machitao <1035937382@qq.com>
2021-10-11 23:27:44 +0800

    feat:第139题空

---
+++ src/main/java/first/OneThreeNine.java
@@ -0,0 +1,16 @@
+package first;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author machitao
+ * @date 2021/10/7
+ * @description
+ */
+public class OneThreeNine {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
