9d1766c9cc3fe8a468ccfb78e331d06fb5172539
machitao <1035937382@qq.com>
2021-11-11 22:45:45 +0800

    feat:添加177题题解

---
+++ src/main/java/first/OneSevenSeven.sql
@@ -0,0 +1,15 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N := N-1;
+RETURN (
+    SELECT
+        salary
+    FROM
+        employee
+    GROUP BY
+        salary
+    ORDER BY
+        salary DESC
+    LIMIT N, 1
+    );
+END
