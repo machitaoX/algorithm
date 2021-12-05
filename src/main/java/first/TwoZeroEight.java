8e74f0e47c49ebeebf51b12d094e2f290c6c50c4
machitao <1035937382@qq.com>
2021-12-05 19:38:32 +0800

    feat:添加208题题解

---
+++ src/main/java/first/TwoZeroEight.java
@@ -0,0 +1,41 @@
+package first;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author machitao
+ * @date 2021/12/5
+ * @description
+ */
+public class TwoZeroEight {
+
+    class Trie {
+        Set<String> items;
+
+        public Trie() {
+            items = new HashSet<>();
+        }
+
+        public void insert(String word) {
+            if (!items.contains(word)) {
+                items.add(word);
+            }
+        }
+
+        public boolean search(String word) {
+            return items.contains(word);
+        }
+
+        public boolean startsWith(String prefix) {
+            Iterator iterator = items.iterator();
+            for (String s : items) {
+                if (s.startsWith(prefix)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
