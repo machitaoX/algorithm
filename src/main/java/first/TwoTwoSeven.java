ff31f8d9394ca846fe361e3eaf1b7d1da83c960b
machitao <1035937382@qq.com>
2021-12-19 13:30:41 +0800

    feat:添加227题题解

--- src/main/java/first/TwoTwoSeven.java
+++ src/main/java/first/TwoTwoSeven.java
@@ -1,5 +1,9 @@
 package first;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @program: algorithm
  * @description:
@@ -8,6 +12,59 @@ package first;
  **/
 public class TwoTwoSeven {
     public int calculate(String s) {
-        return 0;
+        if (s.length() == 1) {
+            return Integer.parseInt(s);
+        }
+
+        Queue<Integer> num = new LinkedList<>();
+        Queue<Character> calculateSignal = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int temp = 0;
+                while (Character.isDigit(c)) {
+                    temp = temp * 10 + c - '0';
+                    i++;
+                    if (i >= s.length()) {
+                        break;
+                    }
+                    c = s.charAt(i);
+                }
+                if (!Character.isDigit(c)) {
+                    i--;
+                }
+                num.offer(temp);
+            } else if (' ' == c) {
+                // do nothing
+            } else {
+                calculateSignal.offer(c);
+            }
+        }
+        int res = num.poll();
+        while (!calculateSignal.isEmpty()) {
+            int first = num.poll();
+            char cal = calculateSignal.poll();
+            if (cal == '*') {
+                res = res * first;
+            } else if (cal == '/') {
+                res = res / first;
+            } else {
+                while (!calculateSignal.isEmpty() && calculateSignal.peek() != '+' && calculateSignal.peek() != '-') {
+                    char newCal = calculateSignal.poll();
+                    int newFirst = num.poll();
+                    if (newCal == '*') {
+                        first = newFirst * first;
+                    } else {
+                        first = first / newFirst;
+                    }
+                }
+                if (cal == '+') {
+                    res = first + res;
+                } else {
+                    res = res - first;
+                }
+            }
+        }
+        return res;
     }
 }
