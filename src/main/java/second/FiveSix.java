c7e8558c83e6b2e4922d7352e98b3f80706f42d7
machitao <1035937382@qq.com>
2022-01-04 09:57:16 +0800

    feat:添加62题题解

--- src/main/java/second/FiveSix.java
+++ src/main/java/second/FiveSix.java
@@ -1,5 +1,10 @@
 package second;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author machitao
  * @date 2021/12/31
@@ -7,6 +12,26 @@ package second;
  */
 public class FiveSix {
     public int[][] merge(int[][] intervals) {
-
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        List<int[]> tempRes = new ArrayList<>();
+        for (int[] i : intervals) {
+            if (tempRes.size() == 0) {
+                int[] temp = new int[]{i[0], i[1]};
+                tempRes.add(temp);
+            } else {
+                int[] last = tempRes.get(tempRes.size() - 1);
+                if (last[1] >= i[0]) {
+                    last[1] = Math.max(last[1], i[1]);
+                } else {
+                    tempRes.add(new int[]{i[0], i[1]});
+                }
+            }
+        }
+        return tempRes.toArray(new int[tempRes.size()][]);
     }
 }
