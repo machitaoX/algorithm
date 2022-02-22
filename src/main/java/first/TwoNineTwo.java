ec552b6b1b9ea03e05f2237611730363450ce456
machitao <1035937382@qq.com>
2022-02-22 22:48:47 +0800

    feat:添加292题题解

---
+++ src/main/java/first/TwoNineTwo.java
@@ -0,0 +1,12 @@
+package first;
+
+/**
+ * @author machitao
+ * @date 2022/2/22
+ * @description
+ */
+public class TwoNineTwo {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
