32fd4f53085bb583abb5fa4dab7f4b59297529f9
machitao <1035937382@qq.com>
2023-05-08 22:13:45 +0800

    feature:309

---
+++ src/main/java/third/ThreeOneNine.java
@@ -0,0 +1,14 @@
+package third;
+
+/**
+ * @program: algorithm
+ * @description:
+ * @author: machitao
+ * @create: 2023-02-15 22:22
+ **/
+public class ThreeOneNine {
+    public int bulbSwitch(int n) {
+
+        return (int)Math.sqrt(n);
+    }
+}
---
+++ src/main/java/third/ThreeTwoFour.java
@@ -0,0 +1,15 @@
+package third;
+
+import java.util.Arrays;
+
+/**
+ * @program: algorithm
+ * @description:
+ * @author: machitao
+ * @create: 2023-02-16 21:48
+ **/
+public class ThreeTwoFour {
+    public void wiggleSort(int[] nums) {
+
+    }
+}
